ae97b1a907a0351a9686c32cbe4d9246f8e24894
wilson fransicius <fransicius.wilson@gmail.com>
2016-10-21 20:12:57 +0700

    "Tears are words the heart can't say"
    SORRY I UPLOAD LATE!
    minor imporvement in the snake cage and create for the user input and food.

--- snakeMain.cpp
+++ snakeMain.cpp
@@ -6,21 +6,51 @@
 // © 2016 Wilson Fransicius
 
 #include <iostream>
+#include <conio.h>					//for the direction input
+#include <stdlib.h>					//for random
 using namespace std;
 
-int width = 75;
-int height = 35;
 
-void Cage();
+int x, y, foodX, foodY;				// X, Y Coordinate
+int width = 75;						// Cage width
+int height = 35;					// Cage height
+
+bool gameOver = false;				// logic
+enum direction {START = 0, LEFT, RIGHT, UP, DOWN}; // for the direction
+direction dir;
+
+void base();
+void cage();
+void snake();
+void logic();
 
 int main(){
-
-	Cage();
+	
+	/*base();
+	while (!gameOver){
+		cage();
+		snake();
+		logic();
+	}
+	*/
+	base();
+	cage();
 	
 	return 0;	
 }
 
-void Cage(){
+
+void base(){					// Base logic
+	
+	gameOver = false;
+	dir = START;
+	x = width/2;
+	y = height/2;
+	foodX = rand() % width;		//Generate random food X coordinate
+	foodY = rand() % height;	//Generate random food Y coordinate
+}
+
+void cage(){
 	// system("cls");
 	for (int i = 0; i < width; i++){
 		
@@ -29,21 +59,30 @@ void Cage(){
 	
 	cout << endl;
 	
-	for (int k = 0; k < height; k++){
+	for (int k = 0; k < height; k++){ // for Y
 		
-		for (int l=0; l < width; l++){
-		
-		if (l == 0){
-			cout << "*";
+		for (int l=0; l < width; l++){ // for X
+		if (l ==0){
+			
+			cout << "x";
 		}
-		
-		else if (l == width-1){
+		if (l == width-2){
 			cout << "x" << endl;
 		}
 		
-		else {
+		
+		if (l == x && k == y){
+			cout << "S";				//Snake starts here
+		}
+		else if (l == foodX && k == foodY){
+			cout << "F";
+		}
+		
+	    else {
 			cout << " ";
 		}
+		
+				
 	
 	}
 	}
@@ -55,4 +94,10 @@ void Cage(){
 	
 }
 
+void snake(){
+}
+
+void logic(){
+	
+}
 
